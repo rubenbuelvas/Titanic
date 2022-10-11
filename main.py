cfa3d57b9c89e87e489d7dba57b1d8014999fd25
rubenbuelvas <rcbuelvasv@gmail.com>
2022-10-10 21:52:00 -0500

    Parse req body and load model

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+import sklearn
+from sklearn.tree import DecisionTreeClassifier
+
+# Model
+
+class Input(BaseModel):
+    sex: str
+    pclass: int
+    age: int
+
+class Output(BaseModel):
+    prediction: float
+
+# Service
+
+def load_model(filename='./model/dt.sav') -> DecisionTreeClassifier:
+    return joblib.load(filename)
+
+
+def parse_request(input: Input) -> pd.DataFrame:
+    data = {'Age': [0.0], 'Pclass_1': [0], 'Pclass_2': [0], 'Pclass_3': [0], 'Sex_female': [0], 'Sex_male': [0]}
+    data['Age'] = float(input.age)
+    data['Pclass_'+input.pclass] = 1
+    data['Sex_'+input.sex] = 1
+    return pd.DataFrame(data=data)
+
+
+
+def predict_service(input: Input) -> Output:
+    input_df = parse_request(input)
+    model = load_model()
+    model.predict(input_df)[0]
+
+# Controller
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/ping')
+async def ping_controller():
+    return 'pong'
+
+
+@app.post('/predict')
+async def predict_controller(input: Input):
+    return predict_service(input)
